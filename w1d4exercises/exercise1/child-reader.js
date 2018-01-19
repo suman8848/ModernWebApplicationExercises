936c3217e97de6774f335b47e53e97644b8e0dd7
Suman <suman.khatiwada@hotmail.com>
2018-01-19 14:13:59 -0600

    updated exercises4

--- w1d4exercises/exercise1/child-reader.js
+++ w1d4exercises/exercise1/child-reader.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
+var data;
 process.on('message', function(filepath){
-    fs.readFile(filepath,(err,data)=>{
-        if(err) throw err;
-        process.send(data.toString());
-    });
+    // fs.readFile(filepath,(err,data)=>{
+    //     if(err) throw err;
+    //     process.send(data.toString());
+    // });
+        console.log("filepath"+ filepath);
+    fs.createReadStream(filepath)
+        .on('data',(dataChunk)=>{
+         data+=dataChunk;
+        })
+        .on('end',()=>{
+            process.send(data);
+    })
 });
--- w1d4exercises/exercise1/fileurl.js
+++ w1d4exercises/exercise1/fileurl.js
@@ -1,4 +1,4 @@
-const rx = require('rxjs/Rx');
+const rx = require('rx');
 const fs  = require('fs');
 const path = require('path');
 const {fork} = require('child_process');
@@ -7,24 +7,25 @@ const url = require('url');
 
 const requests_ = new rx.Subject();
 
-function call(req, res){
-    const myURL = url.parse(req.url,true) ;
+function call(e){
+    const myURL = url.parse(e.req.url,true) ;
 
     var filepath = myURL.query.url;
     if(filepath){
         const reader = fork('child-reader.js');
         reader.send(filepath);
         reader.on('message', msg => {
-            res.end(msg);
+            e.res.end(msg);
         });
     }
 }
 
+requests_.subscribe(call);
+
 const hostname = '127.0.0.1';
 const port = 1337;
 server.createServer((req, res) => {
-    requests_.subscribe(call(req,res));
-    //requests_.next({ req: req, res: res });
+    requests_.onNext({req:req,res:res});
 
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
